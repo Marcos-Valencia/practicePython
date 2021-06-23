c8c1467bdf23ae22cd2705ae669d77516a669fbb
Marcos-Valencia <46791228+Marcos-Valencia@users.noreply.github.com>
2021-06-22 22:41:41 -0700

    added function
    just slapped everything into a working function and making it function properly

--- ex8rps.py
+++ ex8rps.py
@@ -1,7 +1,6 @@
 print("WELCOME TO RPS")
 user1 = str(input("who will be player 1?"))
 user2 = str(input("who will be player 2?"))
-game_status = str("ongoing")
 user1_trn = str(user1) + ", Paper Rock or Scissors?"
 user2_trn = str(user2) + ", Paper Rock or Scissors?"
 
@@ -11,24 +10,25 @@ print("This is RPS(Rock Paper Scissors)\n "
       "Paper = 2\n"
       "Scissors = 3")
 
-while game_status !=str("out"):
-    user1_move = int(input(user1_trn))
-    user2_move = int(input(user2_trn))
-    if user1_move == user2_move:
-        print("draw")
-    elif user1_move ==1 and user2_move == 3:
-        print(user1, " is the winner Congrats")
-    elif user2_move ==1 and user1_move == 3:
-        print(user2, " is the winner Congrats")
-    elif user1_move ==3 and user2_move ==2:
-        print(user1, " is the winner Congrats")
-    elif user2_move ==3 and user1_move ==2:
-        print(user2, " is the winner Congrats")
-    elif user1_move ==2 and user2_move ==1:
-        print(user1, " is the winner Congrats")
-    elif user2_move == 2 and user1_move == 1:
-        print(user2, " is the winner Congrats")
-
-    game_status = str(input("Do you want to quite? Type 'out' if so else type any other character/s \n "))
-
-
+def prs_func(use1, use2):
+    game_status = str("ongoing")
+    while game_status !=str("out"):
+        user1_move = int(input(user1_trn))
+        user2_move = int(input(user2_trn))
+        if user1_move == user2_move:
+            print("draw")
+        elif user1_move ==1 and user2_move == 3:
+            print(user1, " is the winner Congrats")
+        elif user2_move ==1 and user1_move == 3:
+            print(user2, " is the winner Congrats")
+        elif user1_move ==3 and user2_move ==2:
+            print(user1, " is the winner Congrats")
+        elif user2_move ==3 and user1_move ==2:
+            print(user2, " is the winner Congrats")
+        elif user1_move ==2 and user2_move ==1:
+            print(user1, " is the winner Congrats")
+        elif user2_move == 2 and user1_move == 1:
+            print(user2, " is the winner Congrats")
+        game_status = str(input("Do you want to quite? Type 'out' if so else type any other character/s \n "))
+prs_func(user1, user2)
+print("||     !!GAME OVER!!     ||")
